0f1cace90d404a19f7d1e5ede92b7dc85e610bcc
Arbaz <arbazjani8@gmail.com>
2023-05-19 11:52:23 -0700

    Initail commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import Hero from '@/components/hero/hero'
-import About from '@/components/SecondSection/About'
+import About from '@/components/SecondSection/about'
 import Manu from '@/components/3rd/Manu'
 
 export default function Home() {
